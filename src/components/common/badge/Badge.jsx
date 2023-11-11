fb22baab9386db5f539833d0b2f39805331ff4b8
DanielAlinIosif <aliniosifdaniel@gmail.com>
2023-11-11 16:07:31 +0200

    git commit -m "
    Signed-off-by: DanielAlinIosif <aliniosifdaniel@gmail.com>

--- src/components/common/badge/Badge.jsx
+++ src/components/common/badge/Badge.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./badge/Badge.css";
 
 const Badge = (props) => {
-  // eslint-disable-next-line no-unused-vars
   const styleBadge = () => {
     switch (props.status) {
       case "Todo":
@@ -17,9 +16,7 @@ const Badge = (props) => {
         return "grey";
     }
   };
-
-  // eslint-disable-next-line no-template-curly-in-string
-  return <p className={"status ${stylebadge()}"}>{props.status}</p>;
+  return <p className={"status $(stylebadge)"}>{props.status}</p>;
 };
 
 export default Badge;
