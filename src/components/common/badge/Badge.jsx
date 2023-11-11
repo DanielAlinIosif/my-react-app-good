b0bd7dfb468eab9d7ad46bc7cd90d9f0f5403c54
DanielAlinIosif <aliniosifdaniel@gmail.com>
2023-11-11 20:32:49 +0200

    git commit -m "
    Signed-off-by: DanielAlinIosif <aliniosifdaniel@gmail.com>

--- src/components/common/badge/Badge.jsx
+++ src/components/common/badge/Badge.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Badge.css";
 
+// eslint-disable-next-line no-unused-vars
+const BADGE_COLORS = ["grey", "yellow", "blue", "green"]; 
+
 const Badge = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const badgeColors = BADGE_COLORS.includes(props.color) ? props.color: "grey"
   // eslint-disable-next-line no-template-curly-in-string
-  return <p className={"status ${props.color}"}>{props.label}</p>;
+  return <p className={"status ${badgeColors}"}>{props.label}</p>;
 };
 
 export default Badge;
